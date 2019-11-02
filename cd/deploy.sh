8cc098bd6a2bf52195952e305308881d003b4824
Nathan Fischer <nfischer921@gmail.com>
2019-11-02 14:06:45 -0700

    Github package repo

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,4 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	mvn deploy -DskipTests=true -P sign,build-extras --settings cd/mvnsettings.xml
+	mvn deploy -DskipTests=true -Dregistry=https://maven.pkg.github.com/blackdoor -Dtoken=$GH_TOKEN
 fi
